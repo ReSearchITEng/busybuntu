394809d59a4a3e90d66b9edc502c325dd9d17330
ReSearchITEng <ReSearchITEng@gmail.com>
2025-04-01 20:11:35 +0300

    ulimit

--- 14_limits.sh
+++ 14_limits.sh
@@ -7,9 +7,9 @@ echo $0
 
 set -vx
 cat <<EOF | sudo tee /etc/security/limits.d/manual.conf >/dev/null
-* soft nofile 20000
+* soft nofile 200000
 * hard nofile 1048500
-* soft nproc 62796
-* hard nproc 65535
+* soft nproc 300100
+* hard nproc 400100
 EOF
 
