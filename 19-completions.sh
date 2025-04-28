7d6bda23c37c92fe57326d14d6cf4a64a3ab7d48
ReSearchITEng <ReSearchITEng@users.noreply.github.com>
2025-04-28 15:13:23 +0300

    Update 19-completions.sh

--- 19-completions.sh
+++ 19-completions.sh
@@ -14,11 +14,11 @@ REPO=bash-completion
 wget https://api.github.com/repos/$OWNER/$REPO/releases/latest -O - | awk -F \" -v RS="," '/browser_download_url/ {print $(NF-1)}' | xargs wget
 #wget https://github.com/scop/bash-completion/releases/download/2.16.0/bash-completion-2.16.0.tar.xz
 tar -xf bash-completion-*.tar.xz
-cd bash-completion-*
-./configure
+cd bash-completion-*/
+./configure prefix=/usr sysconfdir=/etc
 make prefix=/usr sysconfdir=/etc
 sudo rm -f /etc/bash_completion 2>&- || true
-sudo make install
+sudo make install prefix=/usr sysconfdir=/etc
 cd ${P}
 rm -rf ${work}
 
